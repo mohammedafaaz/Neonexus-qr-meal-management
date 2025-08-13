bf5b7b08bdb2356abaae0609f3ac139f2f48cbba
Md Afaaz <mohammedafaaz433@gmaail.com>
2025-08-13 20:50:53 +0530

    Errors

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qrmealpass",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "Flask>=2.2.5",
+        "Werkzeug>=2.2.3",
+        "Flask-SQLAlchemy>=2.5.1",
+        "Flask-Mail>=0.9.1",
+        "email-validator>=2.1.0",
+        "gunicorn>=20.1.0",
+        "Pillow>=9.5.0",
+        "psycopg2-binary>=2.9.9",
+        "qrcode[pil]>=7.4.2",
+        "SQLAlchemy>=1.4.41,<2.0.0"
+    ],
+    python_requires=">=3.8",
+)
