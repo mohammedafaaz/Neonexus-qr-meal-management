70043b2786ce43fe73e99d78a5ba4feb57dedd01
Md Afaaz <mohammedafaaz433@gmaail.com>
2025-08-17 20:20:13 +0530

    s

--- extensions.py
+++ extensions.py
@@ -1,9 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
 mail = Mail()
+
